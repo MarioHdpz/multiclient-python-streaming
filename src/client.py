6f827598e26e84ad8b0b041d9f8ef6396b85d885
Mario <mariohdpz@gmail.com>
2020-09-20 20:48:14 -0500

    Add ssh tunnel to client connection

--- src/client.py
+++ src/client.py
@@ -1,4 +1,5 @@
 """Streaming client"""
+import os
 import socket
 import time
 
@@ -6,19 +7,27 @@ import cv2
 import imagezmq
 import imutils
 from imutils.video import WebcamVideoStream
+from sshtunnel import open_tunnel
 
-sender = imagezmq.ImageSender(connect_to="tcp://*:5555", REQ_REP=False)
+with open_tunnel(
+    os.getenv("SERVER_HOST"),
+    ssh_username=os.getenv("SERVER_USER"),
+    ssh_password=os.getenv("SERVER_PASSWORD"),
+    remote_bind_address=("127.0.0.1", 5555),
+) as server:
+    port = server.local_bind_port
+    sender = imagezmq.ImageSender(connect_to=f"tcp://127.0.0.1:{port}", REQ_REP=False)
 
-dispositivo = socket.gethostname()
-capture = WebcamVideoStream(src=0).start()
-time.sleep(2.0)
-jpeg_quality = 95
+    dispositivo = socket.gethostname()
+    capture = WebcamVideoStream(src=0).start()
+    time.sleep(2.0)
+    jpeg_quality = 95
 
-while True:
+    while True:
 
-    frame = capture.read()
-    frame = imutils.resize(frame, width=220)
-    ret_code, jpg_buffer = cv2.imencode(
-        ".jpg", frame, [int(cv2.IMWRITE_JPEG_QUALITY), jpeg_quality]
-    )
-    sender.send_jpg(dispositivo, jpg_buffer)
+        frame = capture.read()
+        frame = imutils.resize(frame, width=220)
+        ret_code, jpg_buffer = cv2.imencode(
+            ".jpg", frame, [int(cv2.IMWRITE_JPEG_QUALITY), jpeg_quality]
+        )
+        sender.send_jpg(dispositivo, jpg_buffer)
